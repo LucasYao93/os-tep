dbfd1a9a261d4f81b4138b29f10b333e14904eb5
LucasYao93 <lucas_yaodidi@outlook.com>
2019-12-20 10:50:40 +0000

    NA

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include "core.h"
 
-int sizeof_str();
+int change_char(char *pt)
+{
+  char *char_pt = (char *)malloc(sizeof(char));
+  pt = char_pt;
+  char val = 'Y';
+  *char_pt = val;
+  free(char_pt);
+  return 0;
+}
 
 int main()
 {
-  
+  char *main_pt = (char *)malloc(sizeof(char));
+  char val = 'N';
+  *main_pt = val;
+  printf("val: %c\n", val);  
+  printf("main_pt value: %c\n", *main_pt);
+
+  change_char(main_pt);  
+
+  printf("after val: %c\n", val);  
+  printf("after main_pt value: %c\n", *main_pt); 
   return 0;
 }
 
-int void_pointer(){
-
-  return 0;
-}
-int sizeof_str()
-{
-  char str[] = "ssss";
-  return  sizeof(str);
-}
