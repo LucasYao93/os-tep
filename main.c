5dcd75d66838d020eb59d6c4451fafe61b993f9c
LucasYao93 <lucas_yaodidi@outlook.com>
2019-12-20 09:34:44 +0000

    NA

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int main(void) {
-  printf("Hello World(pid:%d)\n", (int)getpid());
+int sizeof_str();
+
+int main()
+
+{
+
+}
+
+int void_pointer(){
+
   return 0;
+}
+int sizeof_str()
+{
+  char str[] = "ssss";
+  return  sizeof(str);
 }
